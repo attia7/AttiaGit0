c74f09816ba2e0798b0533e31ea8b72249dec598
attia <attia7@msn.com>
2019-10-10 07:36:18 +0300

    ok fb

--- FB.py/store.py
+++ FB.py/store.py
@@ -4,7 +4,7 @@ class Post:
         self.photo_url = photo_url
         self.name = name
         self.body = body
-
+        
 posts =[]
 class PostStore:
     def get_all(self):
@@ -17,41 +17,48 @@ class PostStore:
         for post in posts:
           if post.id == id:
             return post
+            #break
         
     def update(self, id, fields):
-      for post in posts:
-        if post.id == id:       
-          post.name=fields[0]
-          post.body=fields[1]
+      post = self.get_by_id(id)
+      post.name=fields['name']
+      post.body=fields['body']
+      post.photo_url =fields['photo_url']
 
     def delete(self, id):
-      for post in posts:
-        if post.id == id:        
-          posts.remove(post)
+      post = self.get_by_id(id)
+      posts.remove(post)
 
 
 
-post1 = Post(id=1,
-             photo_url='https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=50&w=50',
-             name='Sara',
-             body='Lorem Ipsum')
-post2 = Post(id=2,
-             photo_url='https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=1&h=100&w=100',
-             name='John',
-             body='Lorem Ipsum')
+# post1 = Post(id=1,
+#              photo_url='https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=50&w=50',
+#              name='Sara',
+#              body='Lorem Ipsum')
+# post2 = Post(id=2,
+#              photo_url='https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=1&h=100&w=100',
+#              name='John',
+#              body='Lorem Ipsum')
 
-store = PostStore()
-print('__1__')
-store.get_all()
-store.add(post1)
-print('__2__')
-store.get_all()
-store.add(post2)
-print('__3__')
-store.get_all()
-print(store.get_all()[0].name)
-print(store.get_by_id(2).body)
-updatefilelds = ['name','Ali']
-store.update(1,updatefilelds)
-print(store.get_all()[0].name)
-print(store.get_all())
+# store = PostStore()
+# print('__1__')
+# store.get_all()
+# store.add(post1)
+# print('__2__')
+# store.get_all()
+# store.add(post2)
+# print('__3__')
+# store.get_all()
+# print(store.get_all()[0].name)
+# print(store.get_by_id(2).body)
+
+# updated_fields = {'name': 'Maryam', 
+#                   'photo_url': 'https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=1&h=100&w=100',
+#                   'body': 'Lorem Ipsum'}
+# store.update(3,updated_fields)
+
+# print(store.get_all()[0].name)
+# print(store.get_all())
+
+
+
--- FB.py/store_test.py
+++ FB.py/store_test.py
@@ -1,4 +1,4 @@
-from store import PostStore, Post
+from store import Post, PostStore
 
 post_store = PostStore()
 dummy_posts = [
@@ -12,6 +12,7 @@ dummy_posts = [
          body='Lorem Ipsum'),
 ]
 
+
 def store_should_add_posts():
     for post in dummy_posts:
         post_store.add(post)
@@ -21,14 +22,36 @@ def store_should_add_posts():
 
 
 def get_by_id_should_retrieve_same_object():
-    #second_post = dummy_posts[1]
-    #retrieved_second_post = post_store.get_by_id(2)
+    # second_post = dummy_posts[1]
+    # retrieved_second_post = post_store.get_by_id(2)
 
-    assert dummy_posts[1] is post_store.get_by_id(3)
-    #assert second_post is retrieved_second_post
+    # assert second_post is retrieved_second_post
+    assert dummy_posts[1] is post_store.get_by_id(2)
 
+
+def update_should_modify_object():
+    fields_to_update = {
+        'photo_url': 'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        'name': 'Alex', 
+        'body': 'Interesting topic right here!'
+    }
+
+    # update second post
+    post_store.update(2, fields_to_update)
+
+    assert post_store.get_by_id(2).name == 'Alex'
+    assert post_store.get_by_id(2).body == 'Interesting topic right here!'
+
+
+def delete_should_remove_object():
+    post_store.delete(2)
+
+    assert len(post_store.get_all()) == 1
+    assert post_store.get_all()[0].id == 1
 
 store_should_add_posts()
 get_by_id_should_retrieve_same_object()
+update_should_modify_object()
+delete_should_remove_object()
 
 print('All tests passed successfully!')
